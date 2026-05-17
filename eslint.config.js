16c98dfb39a2891a5d232ae9d6a2e47093e218e4
Alex99887766 <149763426+Alex99887766@users.noreply.github.com>
2026-05-17 07:22:06 +0000

    CI/CD Pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -5,15 +5,12 @@ export default [
     {
         languageOptions: {
             globals: {
-                // Додаємо стандартні об'єкти браузера
                 window: 'readonly',
                 document: 'readonly',
                 navigator: 'readonly',
                 console: 'readonly',
-                // Виправляємо твої помилки:
                 alert: 'readonly',
                 FormData: 'readonly',
-                // Додаємо про всяк випадок для майбутніх лаб
                 fetch: 'readonly',
                 localStorage: 'readonly',
                 import: 'readonly'
@@ -29,6 +26,6 @@ export default [
         }
     },
     {
-        ignores: ['dist/**', 'node_modules/**', 'vite.config.js']
+        ignores: ['dist/**', 'node_modules/**', 'vite.config.js', 'playwright-report/**', 'test-results/**', 'coverage/**', 'test-results/**']
     }
 ];
